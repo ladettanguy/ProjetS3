383b70eb97fed570bfab6e6e23362bd97642fe63
ladettanguy <tanguy.ladet@etu.umontpellier.fr>
2019-12-02 14:37:59 +0100

    Changement d'algo pour la génération de Réseau

--- src/main/java/GenerationGraphe.java
+++ src/main/java/GenerationGraphe.java
@@ -1,33 +1,32 @@
-import java.io.FileReader;
-import java.lang.String;
-import org.json.simple.parser.JSONParser;
-import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
 
 public class GenerationGraphe {
     private static JSONParser parser = new JSONParser();
 
     public static Reseau generer() {
-        String filePath = "";
+        String filePath = "/home/tamikata/IdeaProjects/ProjetS3/Reseau.json";
         Reseau r = null;
-        try (FileReader fichier = new FileReader(ClassLoader.getSystemResource(filePath).getFile()))
+        try
         {
-            JSONObject jsonObject = (JSONObject) parser.parse(fichier);
+            FileReader file = new FileReader("/home/tamikata/IdeaProjects/ProjetS3/Reseau.json");
+            JSONObject jsonObject = (JSONObject) parser.parse(file);
             JSONArray routeurs = (JSONArray) jsonObject.get("routeur");
             r = new Reseau(20);
-
-            Iterator routeurArray = routeurs.iterator();
-            while(routeurArray.hasNext()) r.addRouteur((String) routeurArray.next());
-
+            for (Object routeur: routeurs) r.addRouteur((String) routeur);
             JSONArray arrayConnexion = (JSONArray) jsonObject.get("connexion");
+
             Iterator arrayConnexionIterator = arrayConnexion.iterator();
-            while(arrayConnexionIterator.hasNext()){
+            while(arrayConnexionIterator.hasNext()) {
                 JSONArray connexion = (JSONArray) arrayConnexionIterator.next();
-                if(connexion.size() == 3)
+                if (connexion.size() == 3)
                     for (int j = 0; j < 3; j++)
-                        r.ajouterConnexion((String) connexion.get(0),(String) connexion.get(1),(int)connexion.get(2));
-                else if(connexion.size() == 4)
+                        r.ajouterConnexion((String) connexion.get(0), (String) connexion.get(1), (int) ((long) connexion.get(2)));
+                else if (connexion.size() == 4)
                     for (int j = 0; j < 4; j++)
                         System.out.println("pas encore implémenté");
             }
