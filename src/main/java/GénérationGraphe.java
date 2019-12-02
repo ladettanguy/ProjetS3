47e7705570dddd2d37173dda8033c927a6d6c706
Tanguy <tanguy.ladet@etu.umontpellier.fr>
2019-12-02 09:32:02 +0100

    Changement de la librairy pour le JSON

--- src/main/java/GénérationGraphe.java
+++ src/main/java/GénérationGraphe.java
@@ -1,22 +1,34 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.String;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class GénérationGraphe {
     public static Reseau generer(String mode) {
         JSONParser parser = new JSONParser();
+        String filePath = "/home/tamikata/IdeaProjects/ProjetS3/Reseau.json";
 
+        try (FileReader fichier = new FileReader(ClassLoader.getSystemResource(filePath).getFile())) {
 
-        try {
-            Object obj = parser.parse(new FileReader("/home/tamikata/IdeaProjects/ProjetS3/Reseau.json"));
+            JSONObject jsonObject = (JSONObject) parser.parse(fichier);
 
-            System.out.println("1\n");
+            JSONObject s = (JSONObject) jsonObject.get(mode);
+
+            JSONArray routeurs = (JSONArray) s.get("routeur");
+            Reseau r = new Reseau(20);
+            Iterator i = routeurs.iterator();
+            while(i.hasNext()){
+
+            }
+            System.out.println(s);
+            /*System.out.println("1\n");
 
             JSONObject reseauJson = (JSONObject) obj;
 
@@ -24,14 +36,16 @@ public class GénérationGraphe {
 
             JSONObject s = (JSONObject) reseauJson.get(mode);
 
-            System.out.println(s.routeur+"\n");
+            System.out.println(s+"\n");
 
             // loop array
             JSONArray cars = (JSONArray) reseauJson.get("Routeur");
             Iterator<String> iterator = cars.iterator();
             while (iterator.hasNext()) {
                 System.out.println(iterator.next());
-            }
+            }*/
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
